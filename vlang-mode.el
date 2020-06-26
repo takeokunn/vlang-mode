7ecc6f7714a1bbf7473f2e3412f2128d968322a8
takeokunn <bararararatty@gmail.com>
2020-06-26 16:40:42 +0900

    Update

--- vlang-mode.el
+++ vlang-mode.el
@@ -1,14 +1,19 @@
 (defconst vlang-mode-keywords
-  '("break" "const" "continue" "defer" "else" "enum"
-    "fn" "for" "go" "goto" "if" "import" "in" "interface"
-    "match" "module" "mut" "none" "or" "pub" "return"
-    "struct" "type" "var" "assert" "sizeof" "typeof"))
+  '("break" "const" "continue" "defer" "else" "enum" "fn" "for" "go" "goto" "if" "import" "in" "interface" "match" "module" "none" "or" "pub" "return" "struct" "type"))
 
 (defconst vlang-mode-types
-  '("bool" "string" "i8" "i16" "int" "i64" "i128" "byte"
-    "u16" "u32" "u64" "u128" "rune" "f32" "f64" "size_t"
-    "size_t" "char" "byteptr" "voidptr" "charptr" "any"
-    "any_int" "any_float" "map"))
+  '("bool"
+    "string"
+    "i8" "i16" "int" "i64" "i128"
+    "byte" "u16" "u32" "u64" "u128"
+    "rune"
+    "f32" "f64"
+    "any_int" "any_float"
+    "byteptr" "voidptr" "charptr" "size_t"
+    "any"))
+
+(defconst vlang-mode-attributes
+  '("deprecated" "inline" "typedef"))
 
 (defconst vlang-mode-constants
   '("true" "false"))
@@ -16,8 +21,10 @@
 (defvar vlang-mode-font-lock-keywords
   `((,(regexp-opt vlang-mode-keywords 'symbols) . font-lock-keyword-face)
     (,(regexp-opt vlang-mode-types 'symbols) . font-lock-type-face)
+    (,(regexp-opt vlang-mode-attributes) . font-lock-keyword-face)
     (,(regexp-opt vlang-mode-constants 'symbols) . font-lock-constant-face)
-    ("\\(?:\\<fn\\s-+&?\\(\\(?:\\sw\\|\\s_\\)+\\)\\s-*(\\)" . font-lock-function-name-face)))
+    ("\\(?:\\<fn\\s-+&?\\(\\(?:\\sw\\|\\s_\\)+\\)\\s-*(\\)" . (1 'font-lock-function-name-face))
+    ("\\(`[^`]*`\\)" . font-lock-string-face)))
 
 (defvar vlang-mode-syntax-table
   (let ((table (make-syntax-table)))
@@ -36,7 +43,12 @@
 (define-derived-mode vlang-mode prog-mode "Vlang"
   "Major mode for editing Vang"
   :syntax vlang-mode-syntax-table
+
+  ;; font-lock
+  (setq font-lock-multiline t)
   (setq font-lock-defaults '((vlang-mode-font-lock-keywords)))
+
+  ;; hooks
   (add-hook 'completion-at-point-functions 'vlang-mode-completion-at-point nil 'local))
 
 ;;;###autoload
